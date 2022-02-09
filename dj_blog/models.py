5f35f61437fe687ec746dd7b2c3df5997b70a4e7
samihahesham <samiha1707594@miuegypt.edu.eg>
2022-02-09 20:20:12 +0200

    Category and tag models

--- dj_blog/models.py
+++ dj_blog/models.py
@@ -34,4 +34,13 @@ class Reply(models.Model):
     reply_body=models.CharField(max_length=100)
     reply_time=models.TimeField(null=True)
     user_id=models.ForeignKey(User, on_delete=models.CASCADE)
-    comment_id=models.ForeignKey(Comment, on_delete=models.CASCADE)    
+    comment_id=models.ForeignKey(Comment, on_delete=models.CASCADE) 
+
+class Category(models.Model):
+    cat_namey=models.CharField(max_length=100)
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+
+class PostTags(models.Model):
+    tag=models.CharField(max_length=100)
+    post_id=models.ForeignKey(Post,on_delete=models.CASCADE) 
+
